79561a24bb349449594f68889feb68b3634d69e6
Subhi-c <csubhiksha23@gmail.com>
2024-09-06 19:23:10 +0530

    react-quiz starter

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <main className="main"></main>
+    </div>
+  );
+}
+
+export default App;
